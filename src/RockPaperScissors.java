66d08aad69d91e1a9170ae91fde552a5db050604
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-10-30 11:27:20 +0200

    Bidirectional communication

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.net.*;
 
-// default backlog is 50
-
 public class RockPaperScissors{
     
     private ServerSocket serverSocket;
@@ -15,24 +13,62 @@ public class RockPaperScissors{
         this.port = port;
     }
     
-    public void start() throws IOException{
+    private void openConnection() throws IOException{
+        
         System.out.println("Starting the server at port: " + port);
         System.out.println("Server IP Address: " + address.toString());
-        serverSocket = new ServerSocket(port, 50, address);
+        
+        serverSocket = new ServerSocket(port, 50, address); // default backlog is 50
         
         System.out.println("Waiting for client...");
         
-        Socket client = serverSocket.accept();
+        serverSocket.accept();
         
-        play(client);
+        // listening for connection...
     }
-    public void connect() throws UnknownHostException, IOException{
+    private void clientConnect() throws UnknownHostException, IOException{
         
         System.out.println("Attempting to connect to: " + address.toString() + ":" + port);
+        
         clientSocket = new Socket(address, port);
-        System.out.println("Connection established");
+        
+        System.out.println("Connection established.");
+        
+        // do read and write
+        clientWrite(clientSocket);
+        //serverRead();
+        //serverWrite(clientSocket);
+        clientRead();
     }
-    public void read() throws IOException{
+    
+    private void clientWrite(Socket socket) throws IOException{
+        
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        
+        writer.write("Client to server");
+        writer.flush();
+        writer.close();
+    }
+    private void serverRead() throws IOException{
+        
+        String userInput;
+        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        System.out.println("Response from client: ");
+        while((userInput = input.readLine()) != null){
+            System.out.println(userInput);
+        }
+    }
+    private void serverWrite(Socket socket) throws IOException{
+        
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        
+        writer.write("Server to client");
+        writer.flush();
+        writer.close();
+        
+    }
+    private void clientRead() throws IOException{
         
         String userInput;
         BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -41,16 +77,6 @@ public class RockPaperScissors{
         while((userInput = input.readLine()) != null){
             System.out.println(userInput);
         }
-    }
-    
-    private void play(Socket client) throws IOException{
-        
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        
-        writer.write("Client connected to server.");
-        writer.flush();
-        writer.close();
-        
     }
     
     public static void main(String[] args){
@@ -73,19 +99,23 @@ public class RockPaperScissors{
             try{
                 RockPaperScissors rpsClient = new RockPaperScissors(ipAddress, portNumber);
                 
-                rpsClient.connect();
-                rpsClient.read();
+                rpsClient.clientConnect();
+                
+                // loop
+                rpsClient.clientWrite(null);
+                rpsClient.clientRead();
+                
             }catch(UnknownHostException e){
                 System.out.println(e.getMessage());
             }catch(IOException ex){
                 System.out.println(ex.getMessage());
             }
             
-            // client code ********************************
         }
         else if(args.length == 1){
             
             // server
+            
             portNumber = Integer.parseInt(args[0]);
             try{
                 ipAddress = InetAddress.getByName("127.0.0.1");
@@ -97,7 +127,12 @@ public class RockPaperScissors{
             try{
                 RockPaperScissors rpsServer = new RockPaperScissors(ipAddress, portNumber);
 
-                rpsServer.start();
+                rpsServer.openConnection();
+                
+                // loop
+                rpsServer.serverRead();
+                rpsServer.serverWrite(null);
+                
             }catch(IOException e){
                 System.out.println(e.getMessage());
             }
