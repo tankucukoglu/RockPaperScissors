d8a8da5b1471b16187f44498d91743cb062eab12
Tan Kucukoglu <tan.kucukoglu@gmail.com>
2015-11-01 13:32:43 +0200

    Fixed server termination

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class RockPaperScissors{
     
     static void startServer(int portNumber){
+    	
+    	System.out.println("Waiting for client...");
         
         while(true){
         	
@@ -15,6 +17,11 @@ public class RockPaperScissors{
 	        	
 	            ServerSocket serverSocket = new ServerSocket(portNumber); 
 	            Socket socket = serverSocket.accept();
+	            
+	            if(socket != null){
+	            	System.out.println("Client connected");
+	            }
+	            
             
 	            BufferedReader ed = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	            String tmp = ed.readLine();
@@ -26,7 +33,7 @@ public class RockPaperScissors{
 	            rounds = Integer.parseInt(tokens[0]);
             
 	            String[] userEntries = tmp.split(delims);
-	            for(int i = 1; i < (rounds+1); i++){
+	            for(int i = 1; i < (rounds + 1); i++){
 	                userEntries[i - 1] = tokens[i];
 	            }
             
@@ -107,18 +114,20 @@ public class RockPaperScissors{
 	            // Prints the (random) choices of server and who wins
 	            String msgToClient = "";
 	            for(int i = 0; i < rounds; i++){
-	                msgToClient += ("Round-" + (i + 1) + ":" + "server chooses " + programEntries[i] + " " + resultArray[i] + "/");
+	                msgToClient += ("Round-" + (i + 1) + ": " + "server chooses " + programEntries[i] + " " + resultArray[i] + "/");
 	            }
 	            msgToClient += ("Client: " + client + "/" + "Tie: " + tie + "/" + "Server: " + server);
 	            pr.println(msgToClient);
 	            
 	            serverSocket.close();
-	            
+	            	            
 	        }catch(Exception ex){
-	        	System.out.println(ex.getMessage());
-	        	
+	        	break;
 	        }
 	    }
+        
+        System.out.println("Client disconnected");
+        System.out.println("Server terminating...");
     }
     /* ------------------------------------------ */
     
@@ -126,8 +135,7 @@ public class RockPaperScissors{
     
         Scanner scanner = new Scanner(System.in);
         int rounds = 0;
-        String roundNumber ="a";
-        
+        String roundNumber = "a";
         
         while(!roundNumber.toLowerCase().equals("q")){
 	        try
@@ -143,8 +151,8 @@ public class RockPaperScissors{
 	                System.exit(0);
 	            }else{
 	                rounds = Integer.parseInt(roundNumber);
-	                if(rounds == 0){
-	                    System.out.println("You cannot enter 0. ");
+	                if(rounds <= 0){
+	                    System.out.println("You cannot enter 0 or lower.");
 	                }
 	            }
 	            
@@ -179,14 +187,17 @@ public class RockPaperScissors{
 	            	System.out.println(t);
 	            }
 	            
-	            sock.close();
+	            sock.close();	            
 	            
 	        }
 	        
 	        catch(Exception e){
 	            System.out.println(e.getMessage());
+	            
 	        }
         }
+        
+        System.out.println("Client is terminated");
         
         scanner.close();
     }
